bf94fdaa223c805942dc573e9b2d5ecc92f81bae
Nicholas Murray <murray.nicholas@gmail.com>
2017-09-09 23:14:06 +0100

    Added Redux Thunk And now Saving Records To Server

--- src/reducers/todo.spec.js
+++ src/reducers/todo.spec.js
@@ -2,29 +2,28 @@ import reducer from './todo'
 
 describe('Todo Reducer', () => {
     test('returns a state object', () => {
-        const result = reducer(undefined, {type: 'ANYTHING'})
-        expect(result).toBeDefined()
+      const result = reducer(undefined, {type: 'ANYTHING'})
+      expect(result).toBeDefined()
     })
     
     test('adds a todo', () => {
         const startState = {
             todos: [
-                { id: 1, name: 'Create Static UI', isComplete: true },
-                { id: 2, name: 'Create Initial State', isComplete: true },
-                { id: 3, name: 'Use State to Render UI', isComplete: false },            
+              { id: 1, name: 'Create Static UI', isComplete: true },
+              { id: 2, name: 'Create Initial State', isComplete: true },
+              { id: 3, name: 'Use State to Render UI', isComplete: false },            
             ]
         }
         const expectedState = {
             todos: [
-                { id: 1, name: 'Create Static UI', isComplete: true },
-                { id: 2, name: 'Create Initial State', isComplete: true },
-                { id: 3, name: 'Use State to Render UI', isComplete: false },
-                { id: 4, name: 'Added Todo', isComplete: false },
+              { id: 1, name: 'Create Static UI', isComplete: true },
+              { id: 2, name: 'Create Initial State', isComplete: true },
+              { id: 3, name: 'Use State to Render UI', isComplete: false },
+              { id: 4, name: 'Added Todo', isComplete: false },
             ]
         }
         const action = {type:'TODO_ADD', payload: { id: 4, name: 'Added Todo', isComplete: false }}
         const result = reducer(startState, action)
         expect(result).toEqual(expectedState)
     });
-    
 })
